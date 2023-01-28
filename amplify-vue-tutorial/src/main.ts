b697fe74e7e02d371442788dc7dd51f0962afe8d
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-01-28 14:29:25 +0900

    chore: フロントのamplify設定

--- amplify-vue-tutorial/src/main.ts
+++ amplify-vue-tutorial/src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { Amplify } from 'aws-amplify';
+import awsExport from './aws-exports';
 
 import App from './App.vue';
 import router from './router';
 
 import './assets/main.css';
 
+Amplify.configure(awsExport);
 const app = createApp(App);
 
 app.use(createPinia());
