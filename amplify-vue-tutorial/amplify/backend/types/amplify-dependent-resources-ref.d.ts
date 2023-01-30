71564f9763124c4dbe01ac599152e6e75b466bf4
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-01-30 21:25:16 +0900

    feature: ユーザー認証

--- amplify-vue-tutorial/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify-vue-tutorial/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,28 @@
-export type AmplifyDependentResourcesAttributes = {};
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyvuetutorial": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifyvuetutorial39881499": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "hosting": {
+    "S3AndCloudFront": {
+      "HostingBucketName": "string",
+      "Region": "string",
+      "S3BucketSecureURL": "string",
+      "WebsiteURL": "string"
+    }
+  }
+}
